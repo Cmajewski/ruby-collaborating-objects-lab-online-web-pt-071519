6e0ba1df610d46fbee9da372c95d77d5516f53f0
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-08 19:56:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,10 +17,10 @@ class Song
   end
 
   def self.new_by_filename (file)
-    song_name= file.split(" - ")[1]
-    artist_name_1=file.split(" - ")[0]
-    song=self.new[song_name]
-    song.artist_name=(artist_name_1)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    song.artist_name = song_info[0]
+    song
   end
 
   def artist_name= (name)
