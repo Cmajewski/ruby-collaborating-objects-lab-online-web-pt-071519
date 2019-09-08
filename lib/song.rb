69d308c6b1b1364bee4e50089a67c24765505902
Caitlin Majewski <cmajewski@wesleyan.edu>
2019-09-08 19:51:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,8 +18,9 @@ class Song
 
   def self.new_by_filename (file)
     song_name= file.split(" - ")[1]
-    song.artist_name=file.split(" - ")[0]
-    @song
+    artist_name_1=file.split(" - ")[0]
+    song=self.new[song_name]
+    song.artist_name=(artist_name_1)
   end
 
   def artist_name= (name)
